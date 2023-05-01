24bb9c0ee6332fcc938270623be6f2b25a5dd0b1
Esenaz <esen.azimov@gmail.com>
2023-05-01 11:52:49 +0600

    add authorization

--- src/pages/authorization/index.jsx
+++ src/pages/authorization/index.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react"
+
 export const AuthorizationPage = () => {
-    return (
-        <div>AuthorizationPage</div>
-    )
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    fetch('http://localhost:1717/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then (res => res.json())
+    .then((data) => {
+        localStorage.setItem('token', data.token)
+
+    })
+  }  
+
+  return (
+    <form>
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>
+        Log in
+      </button>
+    </form>
+  )
 }
