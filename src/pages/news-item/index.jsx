4234515bcdddae635173ae0f368b6bebdc7c2e33
RoniLyu <veronika3011vl@gmail.com>
2023-05-02 13:25:08 +0600

    commit my code

--- src/pages/news-item/index.jsx
+++ src/pages/news-item/index.jsx
@@ -5,6 +5,9 @@ import { useParams } from "react-router-dom"
 export const NewsItemPage = () => {
   const {id} = useParams()
   return (
-      <div>NewsItemPage {id}</div>
+      <div>
+        NewsItemPage {id}
+        
+      </div>
   )
 }
